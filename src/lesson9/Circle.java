2594510e23a687c3f403d3911e3f18ebf6f75131
Vova <vovan1008@gmail.com>
2018-12-21 19:27:02 +0200

    MyProject commit

---
+++ src/lesson9/Circle.java
@@ -0,0 +1,41 @@
+package lesson9;
+
+public class Circle extends Shape {
+    private int x,y,radius;
+
+    public Circle(String color, int x, int y, int radius) {
+        super(color);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/lesson9/Shape.java
@@ -0,0 +1,22 @@
+package lesson9;
+
+import java.util.Objects;
+
+public abstract class Shape {
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public abstract void draw();
+
+
+}
